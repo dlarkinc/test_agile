a7f0bc72ecb0d5acad6f335c99a0dd222c9a4d2d
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-06-11 19:45:44 +0100

    added pluto

--- src/random_planet.py
+++ src/random_planet.py
@@ -1,6 +1,6 @@
 import random
 
-planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
 
 print("One of the planets in our solar system is {0}".format(random.choice(planets)))
 
